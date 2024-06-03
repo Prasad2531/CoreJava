3c20eafca8cab25922f1c61b4566e29130024e9f
Prasad <prasadsp2001@gmail.com>
2024-06-03 09:52:32 +0530

    Added LinkedList latest

--- src/main/java/org/example/linkedlist/SLL.java
+++ src/main/java/org/example/linkedlist/SLL.java
@@ -1,8 +1,9 @@
 package org.example.linkedlist;
 
-class Node{
+class Node {
     int value;
     Node next;
+
     public Node(int value) {
         this.value = value;
         this.next = null;
@@ -10,12 +11,12 @@ class Node{
 }
 
 public class SLL {
-    private Node head=null;
-    private void add(int value){
-        if(head==null){
-            head= new Node(value);
-        }
-        else {
+    private Node head = null;
+
+    private void add(int value) {
+        if (head == null) {
+            head = new Node(value);
+        } else {
             Node temp = head;
             while (temp.next != null) {
                 temp = temp.next;
@@ -23,55 +24,59 @@ public class SLL {
             temp.next = new Node(value);
         }
     }
-    private void delete(int index){
-        if(index==0){
-            head=head.next;
-        }
-        else{
+
+    private void delete(int index) {
+        if (index == 0) {
+            head = head.next;
+        } else {
             Node curr = head;
-            for(int i=0;i<index-1;i++){
-                curr=curr.next;
+            for (int i = 0; i < index - 1; i++) {
+                curr = curr.next;
             }
-            if(curr.next!=null){
-                curr.next=curr.next.next;
+            if (curr.next != null) {
+                curr.next = curr.next.next;
             }
 
         }
     }
-    private static void display(Node temp){
-        while(temp.next!=null){
-            System.out.print(temp.value+"->");
-            temp=temp.next;
+
+    private static void display(Node temp) {
+        while (temp.next != null) {
+            System.out.print(temp.value + "->");
+            temp = temp.next;
         }
         System.out.println(temp.value);
     }
 
-    private int length(){
-        int count=0;
-        Node temp=head;
-        while(temp!=null){
+    private int length() {
+        int count = 0;
+        Node temp = head;
+        while (temp != null) {
             count++;
-            temp=temp.next;
+            temp = temp.next;
         }
         return count;
     }
-    private boolean search(int value){
-        Node temp=head;
-        while(temp!=null && temp.value!=value){
-            temp=temp.next;
+
+    private boolean search(int value) {
+        Node temp = head;
+        while (temp != null && temp.value != value) {
+            temp = temp.next;
         }
-        return temp!=null;
+        return temp != null;
     }
-    private static Node reverse(Node curr){
-        Node prev=null;
-        while(curr!=null){
+
+    private static Node reverse(Node curr) {
+        Node prev = null;
+        while (curr != null) {
             Node newNext = curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=newNext;
+            curr.next = prev;
+            prev = curr;
+            curr = newNext;
         }
         return prev;
     }
+
     /*
     To reverse a ll using recursion
     1->2-3->null
@@ -85,50 +90,137 @@ public class SLL {
                                          /
                                         x
      */
-    private static Node recursiveReverse(Node curr){
-        if(curr==null || curr.next==null){
+    private static Node recursiveReverse(Node curr) {
+        if (curr == null || curr.next == null) {
             return curr;
         }
         Node newHead = recursiveReverse(curr.next);
         Node front = curr.next;
-        front.next=curr;
-        curr.next=null;
+        front.next = curr;
+        curr.next = null;
         return newHead;
     }
-    private static int middle(Node temp){
-        Node slow=temp;
-        Node fast=temp;
-        while(fast != null && fast.next != null){
-            fast=fast.next.next;
-            slow=slow.next;
+
+    private static Node middle(Node temp) {
+        Node slow = temp;
+        Node fast = temp;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
         }
-        return slow.value;
+        return slow;
     }
+
     private static Node swapPairs(Node head) {
-        Node slow=head;
-        Node fast=slow.next;
-        while(fast!=null && slow!=null){
+        Node slow = head;
+        Node fast = slow.next;
+        while (fast != null && slow != null) {
             int curr = slow.value;
-            slow.value=fast.value;
-            fast.value=curr;
-            fast=fast.next.next;
-            slow=slow.next.next;
+            slow.value = fast.value;
+            fast.value = curr;
+            fast = fast.next.next;
+            slow = slow.next.next;
         }
         return head;
     }
+
+    private static int loopLength(Node head) {
+        if (head == null || head.next == null) {
+            return 0;
+        }
+        //Add your code here.
+        Node slow = head;
+        Node fast = head;
+
+        boolean flag = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            return 0;
+        } else {
+            int count = 1;
+            Node temp = slow;
+            while (temp.next != slow) {
+                temp = temp.next;
+                count++;
+            }
+
+            return count;
+        }
+    }
+
+    private static boolean palindrome(Node head) {
+        Node mid = middle(head);
+        Node temp = reverse(mid);
+        Node curr = head;
+        while (temp != null) {
+            if (curr.value != temp.value) {
+                return false;
+            }
+            curr = curr.next;
+            temp = temp.next;
+        }
+        return true;
+
+    }
+
     public static void main(String[] args) {
-        SLL newlist = new SLL();
-        SLL second = new SLL();
-        newlist.add(10);
-        newlist.add(20);
-        newlist.add(30);
-        newlist.add(40);
-        second.add(1);
-        second.add(2);
-        second.add(3);
-        second.add(4);
-        display(newlist.head);
-        display(second.head);
+//        SLL newlist = new SLL();
+//        SLL second = new SLL();
+//        newlist.add(10);
+//        newlist.add(20);
+//        newlist.add(30);
+//        newlist.add(40);
+//        second.add(1);
+//        second.add(2);
+//        second.add(3);
+//        second.add(4);
+//        second.add(5);
+//        second.add(6);
+//        second.add(7);
+//        Node cycleNode = null;
+//        Node temp=second.head;
+//        while(temp!=null){
+//            if(temp.value==2){
+//                cycleNode=temp;
+//                break;
+//            }
+//            temp=temp.next;
+//        }
+//        if(cycleNode!=null){
+//            temp=second.head;
+//            while(temp.next!=null){
+//                temp=temp.next;
+//            }
+//            temp.next=cycleNode;
+//        }
+//        System.out.println(loopLength(second.head));
+        SLL third = new SLL();
+        third.add(1);
+        third.add(2);
+//        third.add(3);
+        third.add(2);
+        third.add(1);
+        display(third.head);
+        System.out.print("From middle: ");
+        display(middle(third.head));
+//        display(recursiveReverse(third.head));
+        Node temp2= third.head;
+        System.out.println();
+        Node temp= middle(temp2);
+        display(recursiveReverse(temp));
+        System.out.print("Is Palindrome: ");
+        System.out.println(palindrome(third.head));
+//        display(recursiveReverse(third.head));
+
+//        display(newlist.head);
+//        display(second.head); //check if second is cyclic
 //        System.out.println(newlist.length());
 //        newlist.delete(2);
 //        display(newlist.head);
@@ -136,10 +228,12 @@ public class SLL {
 //        System.out.println(newlist.search(40));
 //        Node temp = reverse(newlist.head);
 //        display(temp);
-//        System.out.println(middle(newlist.head));
-//        System.out.println(middle(second.head));
+//        System.out.println(middle(newlist.head).value);
+//        System.out.println(middle(second.head).value);
 //        display(recursiveReverse(second.head));
-        display(swapPairs(newlist.head));
-        display(swapPairs(second.head));
+//        display(swapPairs(newlist.head));
+//        display(swapPairs(second.head));
+
+
     }
 }
